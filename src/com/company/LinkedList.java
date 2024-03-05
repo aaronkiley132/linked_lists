4724abdc6ea6ccfa7883c7cde79a4ffd9512cec4
18kiley_a <18kiley_a@bexleygs.co.uk>
2024-03-05 14:14:10 +0000

    classwork

---
+++ src/com/company/LinkedList.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class LinkedList {
+    private ListNode head;
+
+    public LinkedList(int[] values) {
+        ListNode n = new ListNode(values[0]);
+        head = n;
+        for (int i = 1; i < values.length; i++) {
+            n.next = new ListNode(values[i]);
+            n = n.next;
+        }
+    }
+
+    public ListNode getHead() {
+        return head;
+    }
+
+    public String toString(){
+        //start at the head and assemble string of the data values
+        ListNode n = getHead();
+        //neater way of building up an output string without wasting memory
+        StringBuilder result = new StringBuilder();
+        while (n != null){
+            result.append(Integer.toString(n.data));
+            n = n.next;
+            if (n != null){
+                result.append(", ");
+            }
+        }
+        return result.toString();
+    }
+
+    public void addItem(int value){
+        // create a new node with the value
+        ListNode newNode = new ListNode(value);
+        // start at the head and step through nodes to the end
+        ListNode n = getHead();
+        while(n.next != null){
+            n = n.next;
+        }
+        // add the new node at this position
+        n.next = newNode;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int [] data = {13, 7, 68};
+        LinkedList list1 = new LinkedList(data);
+        System.out.println(list1);
+        list1.addItem(99);
+        System.out.println(list1);
 
     }
 }

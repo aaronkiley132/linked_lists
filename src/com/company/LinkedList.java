597a3f33f8c3edadde52b8ba077ce3668d959b07
aaronkiley132 <139629281+aaronkiley132@users.noreply.github.com>
2024-03-05 21:05:36 +0000

    implememnted isEmpty

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -42,4 +42,30 @@ public class LinkedList {
         // add the new node at this position
         n.next = newNode;
     }
+
+    public void resetNext(){
+        //TODO start at the begining of the list
+    }
+
+    public boolean hasNext(){
+        boolean result  = true;
+        //TODO true if there is another item in the list
+        return result;
+    }
+
+    public ListNode getNext(){
+        ListNode result;
+        //TODO retrieves an element (the next one) from the list
+        return result;
+    }
+
+    public boolean isEmpty(){
+        boolean result = false;
+        //Ttrue if the list has no elements i.e. the head is null
+        ListNode n = getHead();
+        if ( n == null){
+            result = true;
+        }
+        return result;
+    }
 }
